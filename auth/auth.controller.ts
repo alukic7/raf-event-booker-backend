687e6e7fd43e5672e6ffb32cc7db1d53b87f2c8f
aleksa <alukicdev@gmail.com>
2025-08-14 09:58:25 +0200

    auto

--- auth/auth.controller.ts
+++ auth/auth.controller.ts
@@ -1,28 +1,11 @@
 import Router from 'express'
 import { handleError } from '../lib/http'
 import { SessionService } from '../session/session.service'
-import type { SafeUser } from '../user/user.types'
 import { AuthService } from './auth.service'
 
 const router = Router()
 const authService = new AuthService()
 const sessionService = new SessionService()
-
-// Register user
-router.post('/register', async (req, res) => {
-  try {
-    const { email, password, firstName, lastName } = req.body
-    const registeredUser: SafeUser = await authService.createUser(
-      email,
-      password,
-      firstName,
-      lastName
-    )
-    res.status(201).json(registeredUser)
-  } catch (err) {
-    handleError(res, err)
-  }
-})
 
 // Log in and receive session id in cookie
 router.post('/login', async (req, res) => {
